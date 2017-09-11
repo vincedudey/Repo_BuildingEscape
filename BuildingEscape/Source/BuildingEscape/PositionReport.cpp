9cbbdde570ab4f155e2a2c1533d69a99e8cf5104
Vincent <vzhu@cpp.edu>
2017-09-11 14:48:54 -0700

    Updated position report for X Y Z location, object name retrieve, and added new class OpenDoor

--- BuildingEscape/Source/BuildingEscape/PositionReport.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReport.h"
-
+#include "Gameframework/Actor.h"
 
 // Sets default values for this component's properties
 //constructor
@@ -11,7 +11,7 @@ UPositionReport::UPositionReport()
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true; //this bit of code runs at every single frame
 
-	// ...
+	
 }
 
 
@@ -21,7 +21,10 @@ void UPositionReport::BeginPlay()
 	Super::BeginPlay(); //do the superclass's beginplay first up the inheritance tree
 
 	// ...
-	
+	//macro
+	FString ObjectName = GetOwner()->GetName();
+	FString ObjectPos = GetOwner()->GetTransform().GetLocation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *ObjectName, *ObjectPos); //%s format string, *ObjectName dereference is used as a TCHAR array type
 }
 
 
